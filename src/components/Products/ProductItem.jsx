8102d3c1f0ea01415d19d0c31d457f77868636c0
Hakan Duyar <iamhakanduyar@gmail.com>
2025-01-04 10:25:05 +0300

    component inline css değişken

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,10 +1,21 @@
+const productImgCss = {
+  width: "200px",
+  height: "200px",
+}
+const productInfoCss = {
+  display: "flex",
+        flexDirection: "column"
+}
+
 function ProductItem() {
   return (
     <div className="produc-item">
       <div className="product-image">
-        <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="product image" /> 
+        <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="product image" 
+        style={productImgCss}
+        /> 
       </div>
-      <div className="product-info">
+      <div className="product-info" style={productInfoCss}>
         <strong className="product-title">Mens Casual Premium Slim Fit T-Shirts</strong>
         <span className="product-price">100₺</span>
       </div>
