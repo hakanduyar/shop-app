c8c9798a8f258622f176f94b3e8638ef1ca0393f
Hakan Duyar <iamhakanduyar@gmail.com>
2025-01-04 10:24:25 +0300

    shopp app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Products from "./components/Products/Products";
+
+function App() {
+  return (
+    <div className="app">
+      <Products />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,12 @@
+import ProductItem from "./ProductItem.jsx";
+
+function Products() {
+  return (
+    <div className="products">
+      <h2>Products Component</h2>
+      <ProductItem />
+    </div>
+  );
+}
+
+export default Products;
